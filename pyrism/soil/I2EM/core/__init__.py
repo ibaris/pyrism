f00402ede6ee9f5f983161d41544d9843cc910c3
ibaris <i.baris@outlook.de>
2018-10-21 16:12:12 +0200

    Deleted not used code

--- pyrism/soil/I2EM/core/__init__.py
+++ pyrism/soil/I2EM/core/__init__.py
@@ -1,4 +1,2 @@
 from .corrfun import *
 from .corrfunc_ems import *
-from .auxiliary import (reflection_coefficients, r_transition, Ra_integration, biStatic_coefficient, Ipp,
-                       emsv_integralfunc, shadowing_function)
